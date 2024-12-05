724152c12961029571ddaab0c7491de1ca00d9ef
agat <agathe@agat.dev>
2024-12-05 16:17:23 +0100

    v2.9

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
