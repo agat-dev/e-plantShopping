0f57add8b5ef115cf1906daf4114f2e0ff96eeb9
agat <agathe@agat.dev>
2024-12-05 18:23:28 +0100

    v2.13

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping/",
   plugins: [react()],
   build: {
     outDir: 'dist',
